ba5e2309f9b6949ab7cfc73453256de726d12ad7
Kevin McGrath <kevin.mcgrath@sungardas.com>
2016-05-21 19:58:51 -0400

    status file for condensation

--- condensation.js
+++ condensation.js
@@ -4,38 +4,55 @@ var async = require('async');
 var fetchImages = require('./lib/fetch-images');
 var fs = require('fs-extra');
 var path = require('path');
+var status = require('./.status');
 var stringify = require('json-stable-stringify');
+
 
 module.exports.initialize = function(condensationCb) {
 
-  var ec2 = new AWS.EC2({region:'us-east-1'});
+  var currentEpoch = Math.floor( Date.now() / 1000 );
 
-  var imageMap = {};
+  if (!status.lastFetch || (currentEpoch - status.lastFetch > 86400)) {
 
-  console.log("particles-amazon-ami: Fetching official Amazon Images. This could take some time.");
-  var f = fetchImages.start();
+    var ec2 = new AWS.EC2({region:'us-east-1'});
 
-  f.on('image', function(image) {
-    imageMap[image.Name] = imageMap[image.Name] || {};
-    imageMap[image.Name][image.regionName] = {};
-    imageMap[image.Name][image.regionName].ami = image.ImageId;
-  });
+    var imageMap = {};
+
+    console.log("particles-amazon-ami: Fetching official Amazon Images. This could take some time.");
+    var f = fetchImages.start();
+
+    f.on('image', function(image) {
+      imageMap[image.Name] = imageMap[image.Name] || {};
+      imageMap[image.Name][image.regionName] = {};
+      imageMap[image.Name][image.regionName].ami = image.ImageId;
+    });
 
 
-  f.on('end', function() {
-    async.each(
-      _.keys(imageMap),
-      function(name,cb) {
-        fs.outputFile(
-          path.join('particles','partials','amazon-image-regions',name),
-          stringify(imageMap[name]),
-          cb
-        );
-      },
-      function(err) {
-        console.log("particles-amazon-ami: Finished loading images");
-        condensationCb(err);
-      }
-    );
-  });
+    f.on('end', function() {
+      async.each(
+        _.keys(imageMap),
+        function(name,cb) {
+          fs.outputFile(
+            path.join(__dirname,'particles','partials','amazon-image-regions',name),
+            stringify(imageMap[name]),
+            cb
+          );
+        },
+        function(err) {
+          if (err) return condensationCb(err);
+
+          console.log("particles-amazon-ami: Finished loading images");
+          status.lastFetch = currentEpoch;
+          fs.outputFile(
+            path.join(__dirname,'.status.json'),
+            stringify(status),
+            condensationCb
+          );
+        }
+      );
+    });
+  }
+  else {
+    condensationCb();
+  }
 };
